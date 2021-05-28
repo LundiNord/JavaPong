051df0d89e75d25eb914486ea5f1e8d72f64b41c
LundiNord <lundi.nord@tutanota.de>
2021-05-28 18:25:19 +0200

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 	//System.out.println("Hallo");
-        EventQueue.invokeLater(() -> {
+        EventQueue.invokeLater(() -> {      //keine Ahnung wofür das ist
             App app1 = new App();
             app1.setVisible(true);
         });
