accd2cc30900cd9aa92d83a872ee563370fe28e6
LundiNord <lundi.nord@tutanota.de>
2021-05-28 17:53:08 +0200

    1

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -12,7 +12,7 @@ public class App extends JFrame {
     public void startUI() {
         add(new Board());
 
-        setSize(250, 200);
+        setSize(1000, 600);
         setTitle("Test Appp");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);    //öffnet sich in der Mitte des Bildschirmes
--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -2,10 +2,41 @@ package com.company;
 
 import javax.swing.JPanel;
 
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
+
 public class Board extends JPanel {
 
     public Board() {
+        paintComponent();
 
     }
 
+    @Override   //Ankündigung der Ändern einer Methode der Überklasse
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        drawDonut(g);
+    }
+
+    public void drawDonut(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        RenderingHints rh           //Macht die Objekte schöner
+                = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHints(rh);  //Macht die Objekte schöner
+
+        Dimension size = getSize();     //Höhe/Breite des Fensters
+        double w = size.getWidth();
+        double h = size.getHeight();
+
+        Ellipse2D e = new Ellipse2D.Double(0, 0, 80, 130);      //Zeichnen
+        g2d.setStroke(new BasicStroke(20));
+        g2d.setColor(Color.gray);
+
+    }
 }
