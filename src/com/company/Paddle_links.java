125be82821a261056d3e83ed72dd95b94c228dab
Jan <jan.schlenk@icloud.com>
2021-05-29 14:14:57 +0200

    1

--- src/com/company/Paddle_links.java
+++ src/com/company/Paddle_links.java
@@ -1,4 +1,9 @@
 package com.company;
 //Linkes Paddle
 public class Paddle_links extends Paddles {
+    public Paddle_links(){
+    x=screensize.width/16;
+    y=screensize.height/2;
+    }
+
 }
--- src/com/company/Paddle_rechts.java
+++ src/com/company/Paddle_rechts.java
@@ -2,4 +2,11 @@ package com.company;
 //Rechtes Paddle
 public class Paddle_rechts extends Paddles {
 
+    public Paddle_rechts(){
+
+    x=screensize.width/16+ screensize.width/16*14;
+    y= screensize.height/2;
+    }
+
+
 }
