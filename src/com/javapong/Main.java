acf3f264eb6ebc091fd0d46facbdeaf3879ee4b8
LundiNord <lundi.nord@tutanota.de>
2021-05-29 23:49:50 +0200

    final final Samstag

--- src/com/javapong/Main.java
+++ src/com/javapong/Main.java
@@ -1,6 +1,21 @@
+/* This file is part of JavaPong.
+        JavaPong is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+        JavaPong is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+        You should have received a copy of the GNU General Public License
+        along with JavaPong.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package com.javapong;
 import java.awt.EventQueue;
 import java.util.Scanner;
+
+//JavaPong by Jan,Ricardo,Leon
 
 public class Main {
 
