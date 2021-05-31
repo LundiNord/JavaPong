ac031a6bdfe832f71cd5d1b3ba7566ea82024647
LundiNord <lundi.nord@tutanota.de>
2021-05-31 20:39:58 +0200

    sound

--- src/com/javapong/MenuBoard.java
+++ src/com/javapong/MenuBoard.java
@@ -8,14 +8,16 @@ import java.awt.event.KeyEvent;
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
 
 public class MenuBoard extends JPanel implements ActionListener {
 
     private Font retroFont;
-    private JButton Startbutton;
+    private JButton startbutton1;
+    private JButton startbutton2;
+    private JButton startbutton2online;
+    private JButton exitbutton;
     private boolean menu2;
     private Sound menusound;
 
@@ -24,7 +26,6 @@ public class MenuBoard extends JPanel implements ActionListener {
 
     public MenuBoard() throws Exception {
         startFont();
-        Startbutton();
         addKeyListener(new MenuBoard.BAdapter());       //Initialisiert Key listener
         setBackground(Color.black);
         setFocusable(true);
@@ -38,7 +39,10 @@ public class MenuBoard extends JPanel implements ActionListener {
         paintTitle(g2d);
         paintSubtitle(g2d);
         if(menu2==false) {
-            paintStartbutton();
+            paintStartbutton1();
+            paintStartbutton2();
+            paintStartbutton2online();
+            paintExitButton();
         }
     }
 
@@ -47,7 +51,6 @@ public class MenuBoard extends JPanel implements ActionListener {
         g2d.setFont(retroFont);
         g2d.drawString("Java-Pong",30,75);
     }
-
     public void paintSubtitle(Graphics2D g2d){
         g2d.setColor(Color.white);
         Font kretrofont= retroFont.deriveFont(Font.PLAIN,20);
@@ -57,12 +60,61 @@ public class MenuBoard extends JPanel implements ActionListener {
         g2d.drawString("by Ricardo, Léon und Jan",100,100);
     }
 
-    public void paintStartbutton(){
+    public void paintStartbutton1(){        //Einzelspieler Start
+        startbutton1 = new JButton();
+        startbutton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Start");
 
-        Startbutton.setText("Start");
-        Startbutton.setSize(100,50);
-        Startbutton.setLocation(200,200);
-        add(initButton(Startbutton));
+            }
+        });
+        startbutton1.setText("Start");
+        startbutton1.setSize(100,50);
+        startbutton1.setLocation(200,200);
+        add(initButton(startbutton1));
+    }
+    public void paintStartbutton2() {           //lokaler Multiplayer Button
+        startbutton2 = new JButton();
+        startbutton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Start");
+
+            }
+        });
+        startbutton2.setText("Start");
+        startbutton2.setSize(100,50);
+        startbutton2.setLocation(300,200);
+        add(initButton(startbutton2));
+    }
+    public void paintStartbutton2online() {         //OnlineMultiplayer Button
+        startbutton2online = new JButton();
+        startbutton2online.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Start");
+
+            }
+        });
+        startbutton2online.setText("Start");
+        startbutton2online.setSize(400,50);
+        startbutton2online.setLocation(200,200);
+        add(initButton(startbutton2online));
+    }
+    public void paintExitButton() {
+        exitbutton = new JButton();
+        exitbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Start");
+
+            }
+        });
+        exitbutton.setText("Start");
+        exitbutton.setSize(500,50);
+        exitbutton.setLocation(200,200);
+        add(initButton(exitbutton));
     }
 
     public JButton initButton(JButton button1) {        //Init Button
@@ -100,16 +152,6 @@ public class MenuBoard extends JPanel implements ActionListener {
 
     }
 
-    public void Startbutton() {
-            Startbutton= new JButton();
-            Startbutton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JOptionPane.showMessageDialog(null, "Start");
-
-                }
-            });
-    }
     public void Menusoundstarten() throws Exception {
         menusound = new Sound("src/resources/sound/198896__bone666138__8-bit-circus-music.wav",20,true);
         menusound.playSound1();
