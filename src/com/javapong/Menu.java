231011d3b01a1190dcc2ad3d2bccd521a392a4fc
Jan <jan.schlenk@icloud.com>
2021-06-01 20:12:54 +0200

    1

--- src/com/javapong/Menu.java
+++ src/com/javapong/Menu.java
@@ -51,11 +51,14 @@ public class Menu extends JFrame {
                 dispose();
                 System.exit(0);                 //Programm beenden
             }
-            if (spiel1.getSpielstopp()==true){
-                try {
-                    menuBoard1.Menusoundstarten();
-                } catch (Exception exception) {
-                    exception.printStackTrace();
+            if(spiel1!=null) {
+                if (spiel1.getSpielstopp() == true) {
+                    try {
+                        menuBoard1.Menusoundstarten();
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+                spiel1=null;
                 }
             }
         }
