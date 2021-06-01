758e4d699c5f25bda9522a98292a41391ffa08bc
Denaires <Ricardo@bandeira.de>
2021-06-01 17:46:16 +0200

    kk

--- src/com/javapong/MenuBoardNeu.java
+++ src/com/javapong/MenuBoardNeu.java
@@ -45,28 +45,42 @@ public class MenuBoardNeu extends JPanel implements ActionListener {
             paintStartbutton2();
             paintStartbutton2online();
             paintExitbutton();
+            paintButton(g2d);
         }
     }
 
     public void paintTitle(Graphics2D g2d){
         g2d.setColor(Color.white);
-        g2d.setFont(retroFont);
-        g2d.drawString("Java-Pong",30,75);
-    }
-    public void paintSubtitle(Graphics2D g2d){
-        g2d.setColor(Color.white);
-        Font kretrofont= retroFont.deriveFont(Font.PLAIN,20);
+        Font kretrofont= retroFont.deriveFont(Font.PLAIN,70);
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         ge.registerFont(kretrofont);
         g2d.setFont(kretrofont);
+        g2d.drawString("Java-Pong",30,75);
+    }
+
+    public void paintSubtitle(Graphics2D g2d){
+        g2d.setColor(Color.white);
+        g2d.setFont(retroFont);
         g2d.drawString("by Ricardo, Léon und Jan",100,100);
+    }
+
+    public void paintButton(Graphics2D g2d){
+        g2d.setColor(Color.white);
+        Font kretrofont= retroFont.deriveFont(Font.PLAIN,15);
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(kretrofont);
+        g2d.setFont(kretrofont);
+        g2d.drawString("Einzelspieler",250,190);
+        g2d.drawString("Lokaler Mehrspieler",200,265);
+        g2d.drawString("Online Modus",255,340);
+        g2d.drawString("Beenden",290,415);
     }
 
     public void paintStartbutton(){
         Startbutton.setVisible(true);
-        Startbutton.setText("Start");
-        Startbutton.setSize(100,50);
-        Startbutton.setLocation(200,200);
+        //Startbutton.setText("Start");
+        Startbutton.setSize(205,60);
+        Startbutton.setLocation(245,150);
         Startbutton.setOpaque(false);
         Startbutton.setContentAreaFilled(false);
         Startbutton.setBorderPainted(false);
@@ -74,9 +88,9 @@ public class MenuBoardNeu extends JPanel implements ActionListener {
     }
     public void paintStartbutton2(){
         Startbutton2.setVisible(true);
-        Startbutton2.setText("Start2");
-        Startbutton2.setSize(100,50);
-        Startbutton2.setLocation(300,200);
+        //Startbutton2.setText("Start2");
+        Startbutton2.setSize(300,60);
+        Startbutton2.setLocation(195,225);
         Startbutton2.setOpaque(false);
         Startbutton2.setContentAreaFilled(false);
         Startbutton2.setBorderPainted(false);
@@ -84,9 +98,9 @@ public class MenuBoardNeu extends JPanel implements ActionListener {
     }
     public void paintStartbutton2online(){
         Startbutton2online.setVisible(true);
-        Startbutton2online.setText("Start2o");
-        Startbutton2online.setSize(100,50);
-        Startbutton2online.setLocation(400,200);
+        //Startbutton2online.setText("Start2o");
+        Startbutton2online.setSize(190,60);
+        Startbutton2online.setLocation(250,300);
         Startbutton2online.setOpaque(false);
         Startbutton2online.setContentAreaFilled(false);
         Startbutton2online.setBorderPainted(false);
@@ -94,9 +108,9 @@ public class MenuBoardNeu extends JPanel implements ActionListener {
     }
     public void paintExitbutton(){
         ExitButton.setVisible(true);
-        ExitButton.setText("Exit");
-        ExitButton.setSize(100,50);
-        ExitButton.setLocation(500,200);
+        //ExitButton.setText("Exit");
+        ExitButton.setSize(130,60);
+        ExitButton.setLocation(275,375);
         ExitButton.setOpaque(false);
         ExitButton.setContentAreaFilled(false);
         ExitButton.setBorderPainted(false);
@@ -105,7 +119,7 @@ public class MenuBoardNeu extends JPanel implements ActionListener {
     public void startFont() throws IOException, FontFormatException {       //Custom Font init
         File f = new File("src/resources/font/PressStart2P.ttf");       //Pfad zu .ttf File
         Font PressStart = Font.createFont(Font.TRUETYPE_FONT,f);                //Neue Font machen
-        PressStart = PressStart.deriveFont(Font.PLAIN,70);                  //Größe, ... festlegen
+        PressStart = PressStart.deriveFont(Font.PLAIN,20);                  //Größe, ... festlegen
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         ge.registerFont(PressStart);                                            //keine Ahnung
         retroFont = PressStart;                                                 //Abspeichern
